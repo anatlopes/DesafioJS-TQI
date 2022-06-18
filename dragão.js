158fe0129ceccb57c37448ca4431afc8d4b41221
Ana Lopes <apt.lopes@gmail.com>
2022-06-18 12:19:20 -0300

    desafiosJS

---
+++ dragão.js
@@ -0,0 +1,18 @@
+let input =  gets( );
+
+
+let qtdEntradas = parseInt(input);
+let nivelEnergia;
+
+
+for( let i = 0 ; i < qtdEntradas ; i++ ){
+    input = gets( );
+    nivelEnergia = parseInt(input);
+    
+    if(  nivelEnergia > 8000    ){
+        print (    'Mais de 8000!'    ) 
+        
+    }  else {
+        print (   'Inseto!'   )
+    }
+}
---
+++ somaNúmerosNaturais.js
@@ -0,0 +1,4 @@
+const A = parseInt(gets());
+const B = parseInt(gets());
+const total = ((B-A+1)*((A+B)/2));
+print(total);
